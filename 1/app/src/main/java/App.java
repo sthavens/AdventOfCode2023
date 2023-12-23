a5b97e54e592e4429ed79138e16a6e4f6cb0e372
Seyd Havens <seyd.havens@gmail.com>
2023-12-23 11:57:24 -0800

    Added in previous work on first day of Advent 2023

---
+++ 1/app/src/main/java/App.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Stream;;
+import java.util.stream.Collectors;
+
+
+public class App {
+    public static void main(String[] args) {
+        File file = new File("/home/sthavens/advent/1/app/src/main/java/source1.txt");
+        try{
+            Scanner scanner = new Scanner(file);
+
+            int sum = 0;
+
+            Map<String, String> nums = Stream.of(new String[][] {
+                {"one", "1"},
+                {"two", "2"},
+                {"three", "3"},
+                {"four", "4"},
+                {"five", "5"},
+                {"six", "6"},
+                {"seven", "7"},
+                {"eight", "8"},
+                {"nine", "9"}
+            }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (Map.Entry<String, String> entry : nums.entrySet()) {
+                    line = line.replaceAll(entry.getKey(), entry.getValue());
+                }
+                System.out.println(line);
+                char first = ' ';
+                char last = ' ';
+                for (int i = 0; i < line.length(); i++) {
+                    char test = line.charAt(i);
+                    if (test > 47 && test < 58) {
+                        first = test;
+                        break;
+                    }
+                }
+                for (int i = line.length() - 1; i >= 0 ; i--) {
+                    char test = line.charAt(i);
+                    if (test > 47 && test < 58) {
+                        last = test;
+                        break;
+                    }
+                }
+                String number = String.valueOf(first) + String.valueOf(last);
+                sum += Integer.parseInt(number);
+            }
+            System.out.println(sum);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
